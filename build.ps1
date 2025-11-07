4fe6a46f2f974342a177f8fbe4ebb6569e5723e4
Warren Buckley <warren@hackmakedo.com>
2025-11-07 16:50:55 +0000

    Bumped patch version to 1.0.3

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 $projectFile = "./Umbraco.Community.User2FA/Umbraco.Community.User2FA.csproj"
 $configuration = "Release"
 $outputDirectory = "./build.out"
-$version = "1.0.2"
+$version = "1.0.3"
 $packageJsonPath = "./Umbraco.Community.User2FA/wwwroot/umbraco-package.json"
 
 # Delete all files in the output directory
